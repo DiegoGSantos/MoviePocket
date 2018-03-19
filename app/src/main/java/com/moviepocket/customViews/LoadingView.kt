c1f4ec8eccea85d4a49bfb49b0b9e201519d0618
Diego Santos <diego.net10@hotmail.com>
2018-03-19 18:54:32 -0300

    Added loading view

---
+++ app/src/main/java/com/moviepocket/customViews/LoadingView.kt
@@ -0,0 +1,33 @@
+package com.moviepocket.customViews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import com.moviepocket.R
+import kotlinx.android.synthetic.main.loading_view.view.*
+
+/**
+ * Created by diego.santos on 19/03/18.
+ */
+class LoadingView : LinearLayout {
+
+    constructor(context: Context) : super(context) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initView()
+    }
+
+    fun initView(){
+        inflate(getContext(), R.layout.loading_view, this);
+
+        val rotation = AnimationUtils.loadAnimation(this.context, R.anim.rotate_animation)
+        loadingProgress.startAnimation(rotation)
+    }
+}
